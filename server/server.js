0d6b5184b99696f7c03b85c71fc358b237c62eed
abhinT <abhintomar1@gmail.com>
2022-09-19 14:44:02 -0400

    Added client folder

--- server/server.js
+++ server/server.js
@@ -11,10 +11,6 @@ app.use(function (req, res, next) {
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.get("/api", (req, res) => {
-    res.json({name: "Abhin"});
-})
-
 app.post("/api", (req, res) => {
     const imgSource = req.body.image;
     axios.get(`https://serpapi.com/search.json?engine=google_reverse_image&image_url=${imgSource}&api_key=0f3db242ed06c9cd4b3eed9b2042812598a2164ec8edfa434fbf6fb23cc0608b`).then((response) => {
@@ -44,5 +40,5 @@ app.post("/api", (req, res) => {
 })
 
 app.listen(5000, () => {
-    console.log("Server started on port 5000");
+    console.log("Server started");
 })
